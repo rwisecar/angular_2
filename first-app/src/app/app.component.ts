0a8b9b84f6e87140583884bd727533969f779bc7
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-03-08 13:17:23 -0800

    more with component lifecycles, modifying bound values on click

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -5,11 +5,17 @@ import { LifecycleComponent } from './lifecycle.component';
   selector: 'app-root',
   template: `
     <h1>Root Component</h1>
-    <fa-lifecycle *ngIf="!delete"></fa-lifecycle>
+    <fa-lifecycle *ngIf="!delete" [bindable]="boundValue">
+        <p>{{ test }}</p>
+    </fa-lifecycle>
     <button (click)="delete = true"> Destruction </button>
+    <button (click)="test = 'Changed Value'"> Click to Change Content</button>
+    <button (click)="boundValue = 2000"> Click to Change Binding</button>
   `
 })
 export class AppComponent {
   title = 'Rachael\'s App';
   delete = false;
+  test = "Starting Value";
+  boundValue = 1000;
 }
--- first-app/src/app/lifecycle.component.ts
+++ first-app/src/app/lifecycle.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, Input } from '@angular/core';
 
 @Component({
   selector: 'fa-lifecycle',
   template: `
-    <p>
-      lifecycle Works!
-    </p>
+    <ng-content></ng-content>
+    <hr>
+    <p>{{ bindable }}</p>
   `,
   styles: []
 })
 export class LifecycleComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  @Input() bindable = 1000;
 
   constructor() { }
 
@@ -18,31 +20,31 @@ export class LifecycleComponent implements OnChanges, OnInit, DoCheck, AfterCont
   }
 
   ngOnInit() {
-      this.log('ngOnChanges');
+      this.log('ngOnInit');
   }
 
   ngDoCheck(){
-      this.log('ngOnChanges');
+      this.log('ngDoCheck');
   }
 
   ngAfterContentInit() {
-      this.log('ngOnChanges');
+      this.log('ngAfterContentInit');
   }
 
   ngAfterContentChecked() {
-      this.log('ngOnChanges');
+      this.log('ngAfterContentChecked');
   }
 
   ngAfterViewInit(){
-      this.log('ngOnChanges');
+      this.log('ngAfterViewInit');
   }
 
   ngAfterViewChecked(){
-      this.log('ngOnChanges');
+      this.log('ngAfterViewChecked');
   }
 
   ngOnDestroy(){
-      this.log('ngOnChanges');
+      this.log('ngOnDestroy');
   }
 
   private log(hook: string){
