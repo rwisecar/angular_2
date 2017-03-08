8e7495125ac202b4ff9f0da84dad33abf1559949
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-03-08 10:37:53 -0800

    removing unnecessary files

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -4,18 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>Inline Template</h1>
-    <fa-other></fa-other>
-    <fa-another>
-        <div>
-            <h1>Hello world</h1>
-        </div>
-    </fa-another>
-  `,
-  styles: [`
-      h1 {
-          color: red;
-      }
-  `]
+  `
 })
 export class AppComponent {
   title = 'Rachael\'s App';
